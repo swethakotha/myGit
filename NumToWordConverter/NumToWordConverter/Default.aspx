723d330533530deca1b2073575a99940357ac768
swethakotha <kothaswe@gmail.com>
2019-06-17 10:39:01 +0530

    WordConvertercode

---
+++ NumToWordConverter/NumToWordConverter/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NumToWordConverter._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h1>Number to word converter</h1>
+        <p class="lead">Please enter the number: 
+            <asp:TextBox ID="TextBox1" runat="server" MaxLength="9"></asp:TextBox>
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1"
+    ControlToValidate="TextBox1" runat="server"
+    ErrorMessage="Only Numbers allowed"
+    ValidationExpression="\d+">
+</asp:RegularExpressionValidator>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </p>
+        <p>
+            <asp:Label ID="ResultVal" runat="server" Text="Label"  Visible="false"></asp:Label>
+        </p>
+    </div>
+   <script>
+$(document).ready(function(){
+    $("#<%=Button1.ClientID %>").click(function () {
+        var txt = $('#<%=TextBox1.ClientID %>');
+        if (txt.val() == null || txt.val() == '') {
+            alert('Please enter a number');
+            return false;
+        }
+  });
+});
+</script>
+
+</asp:Content>
---
+++ NumToWordConverter/NumToWordConverter/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NumToWordConverter
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            long textVal1 = Convert.ToInt64(TextBox1.Text);
+           string wordText = NumberToWords(textVal1);
+            ResultVal.Visible = true;
+            ResultVal.Text = wordText;
+        }
+        public static string NumberToWords(long number)
+        {
+            if (number == 0)
+                return "Zero";
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " Million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var tensMap = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Sseventy", "Eighty", "Ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+    }
+}
